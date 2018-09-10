21c3a23ce704fc01f5d2e6c315f945952847ebf9
Chit <ball4128ok@gmail.com>
2018-09-10 08:03:24 +0700

    starting mvc movie project (again)

---
+++ MvcMovie/Controllers/LoginsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class LoginsController : Controller
+    {
+        private LoginDBContext db = new LoginDBContext();
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Login logins)
+        {
+            /*
+            var UserNameList = new List<string>();
+            var UserNameQry = from d in db.Logins
+                              select d.UserName;
+               */
+            if (ModelState.IsValid)     /*more detail see https://www.c-sharpcorner.com/article/simple-login-application-using-Asp-Net-mvc/ */
+            {
+                var obj = db.Logins.Where(a => a.UserName.Equals(logins.UserName) && a.UserPassword.Equals(logins.UserPassword)).FirstOrDefault();
+                if(obj != null)
+                {
+                    Session["ID"] = obj.ID.ToString();
+                    Session["UserName"] = obj.UserName.ToString();
+                    //Session["UserPassword"] = obj.UserPassword.ToString();
+                    return RedirectToAction("UserDashBoard");
+                }
+            }
+            
+            return View(logins); 
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        // GET: Logins
+        public ActionResult List()
+        {
+            return View(db.Logins.ToList());
+        }
+
+        // GET: Logins/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // GET: Logins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Logins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,UserName,UserPassword,Email,UserPasswordSecurity,ReleaseDate")] Login login)
+        {
+            DateTime dtnow = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                db.Logins.Add(login);
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+
+            return View(login);
+        }
+
+        // GET: Logins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // POST: Logins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,UserName,UserPassword,Email,UserPasswordSecurity,ReleaseDate")] Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(login).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(login);
+        }
+
+        // GET: Logins/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Login login = db.Logins.Find(id);
+            if (login == null)
+            {
+                return HttpNotFound();
+            }
+            return View(login);
+        }
+
+        // POST: Logins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Login login = db.Logins.Find(id);
+            db.Logins.Remove(login); 
+            db.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MvcMovie/Models/Login.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcMovie.Models
+{
+    public class Login
+    {
+
+        public int ID { get; set; }
+
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z'\s]+[0-9]*$")]
+        [RegularExpression(@"^[a-zA-Z][a-zA-Z0-9_]+$")]
+        [MinLength(6)]
+        [MaxLength(15)]
+        public string UserName { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z][a-zA-Z0-9_]+$")]
+        [MinLength(6)]
+        [MaxLength(15)]
+        public string UserPassword { get; set; }
+
+        [StringLength(40)]
+        public string Email { get; set; }
+
+        [RegularExpression(@"^[0-9]*$")]
+        [StringLength(4)]
+        public string UserPasswordSecurity { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}")] // ,ApplyFormatInEditMode = false ,HtmlEncode = false
+        public DateTime ReleaseDate { get; set; }
+
+
+    }
+    public class LoginDBContext : DbContext
+    {
+        public DbSet<Login> Logins { get; set; }
+    }
+}
